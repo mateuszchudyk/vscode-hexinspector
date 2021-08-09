417c5dabbe8975f473e5033dd62307e53b05f037
Mateusz Chudyk <mateuszchudyk@gmail.com>
2021-08-09 20:58:41 +0200

    Fix addSeparatorToNumber

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@
 export function addSeparatorToNumber(str: string, sep: string, n: number) {
     var result = '';
     for (let i = 0; i < str.length; i++) {
-        if (i > 0 && i % n == 0) {
+        if (i > 0 && i % n == 0 && str[str.length - 1 - i] != '-') {
             result = sep + result;
         }
         result = str[str.length - 1 - i] + result;
